e744c5c8d6786a88dc7cd4203ee69cf11e29fb37
mocanuandrei1 <127875795+mocanuandrei1@users.noreply.github.com>
2023-06-08 13:35:18 +0300

    Added the random joke functionality.

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("test");
+const badJokes = ['Why did the deer go to the dentist? It had buck teeth.', 'Why did the computer get glasses? It wanted to improve its website.', 'Did you hear about the medieval lamp? Its a knight light.', 
+'Where do learn how to make ice cream? At sundae school.'];
+
+const randomJoke = jokesArray => {
+    randomNumber = Math.floor(Math.random() * jokesArray.length);
+
+    return jokesArray[randomNumber];
+}
+
+console.log(randomJoke(badJokes));
